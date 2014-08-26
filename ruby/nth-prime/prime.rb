d98f422626be5f2fa065b853fab2af32b3842add
Josh Hill <jhill@globalpersonals.co.uk>
2014-08-26 13:26:29 +0100

    Second iteration of nth-prime

--- ruby/nth-prime/prime.rb
+++ ruby/nth-prime/prime.rb
@@ -1,16 +1,8 @@
+require 'prime'
+
 class Prime
-  def self.nth(n)
-    case n
-    when 0
-      raise ArgumentError
-    when 1
-      2
-    when 2
-      3
-    when 6
-      13
-    when 10001
-      104743
-    end
+  def nth(n)
+    raise ArgumentError if n == 0
+    Prime.first(n).last
   end
 end
