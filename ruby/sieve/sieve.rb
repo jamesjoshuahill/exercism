bbb5eacc543cc39dcc9311f1b4d0cdff351782f3
Josh Hill <jhill@globalpersonals.co.uk>
2014-08-28 17:27:52 +0100

    Possible variation of sieve

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -3,17 +3,32 @@ require 'prime'
 class Sieve
   def initialize(limit)
     @limit = limit
+    @numbers = (0..@limit).to_a
   end
 
   def primes
-    numbers = [nil, nil, *2..@limit]
-    numbers.each do |number|
+    @primes ||= sieve
+  end
+
+  private
+
+  def nil_zero_and_one
+    @numbers[0] = @numbers[1] = nil
+  end
+
+  def nil_multiples_of(number)
+    (number**2).step(by: number, to: @limit) do |multiple|
+      @numbers[multiple] = nil
+    end
+  end
+
+  def sieve
+    nil_zero_and_one
+    @numbers.each do |number|
       next if number.nil?
       break if number**2 > @limit
-      (number**2).step(by: number, to: @limit) do |multiple|
-        numbers[multiple] = nil
-      end
+      nil_multiples_of(number)
     end
-    numbers.compact
+    @numbers.compact
   end
 end
