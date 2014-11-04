733d419afd5963e52145651e14ea8c53d15911d7
Josh Hill <jhill@globalpersonals.co.uk>
2014-11-04 17:49:31 +0000

    Third iteration of beer-song

--- ruby/beer-song/beer_song.rb
+++ ruby/beer-song/beer_song.rb
@@ -1,47 +1,54 @@
+module SentenceCase
+  refine String do
+    def sentence_case
+      self[0].upcase << self[1..length]
+    end
+  end
+end
+
+using SentenceCase
+
 class BeerSong
   def verse(number)
-    "#{bottles(number, :sentence_case)} on the wall, #{bottles(number)}.\n" <<
-    "#{action(number)}, #{bottles(number - 1)} on the wall.\n"
+    Verse.say(number)
   end
 
   def verses(from, to)
-    from.downto(to).map { |number| verse(number) + "\n" }.join
+    from.downto(to).each_with_object("") do |number, verses|
+      verses << verse(number) << "\n"
+    end
   end
 
   def sing
     verses(99, 0)
   end
+end
 
-  private
-
-  def bottle_number(number, format = nil)
-    case number
-    when -1
-      99
-    when 0
-      format == :sentence_case ? "No more" : "no more"
-    else
-      number
-    end
+module Verse
+  def self.say(number)
+    "#{BottlesOfBeer.say(number).sentence_case} on the wall, #{BottlesOfBeer.say(number)}.\n" <<
+    "#{action(number)}, #{BottlesOfBeer.say(number - 1)} on the wall.\n"
   end
 
-  def bottle_unit(number)
-    number == 1 ? "bottle" : "bottles"
+  def self.action(number)
+    return "Go to the store and buy some more" if number.zero?
+    pronoun = number == 1 ? "it" : "one"
+    "Take #{pronoun} down and pass it around"
+  end
+end
+
+module BottlesOfBeer
+  def self.say(quantity)
+    "#{number(quantity)} #{unit(quantity)} of beer"
   end
 
-  def bottles(number, format = nil)
-    "#{bottle_number(number, format)} #{bottle_unit(number)} of beer"
+  def self.number(quantity)
+    return 99 if quantity == -1
+    return "no more" if quantity.zero?
+    quantity
   end
 
-  def pronoun(number)
-    number == 1 ? "it" : "one"
-  end
-
-  def action(number)
-    if number.zero?
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun(number)} down and pass it around"
-    end
+  def self.unit(quantity)
+    quantity == 1 ? "bottle" : "bottles"
   end
 end
