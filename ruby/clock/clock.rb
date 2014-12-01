69de8214dea5b9e5778c4f427acb26e0f5dd9e15
Josh Hill <jhill@globalpersonals.co.uk>
2014-12-01 14:52:48 +0000

    Second iteration of clock

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,44 +1,33 @@
 class Clock
+  HOURS_PER_DAY = 24
+  MINUTES_PER_HOUR = 60
+  MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR
+
   def self.at(hour, minute = 0)
-    new(hour, minute)
+    new(hour: hour, minute: minute)
   end
 
-  def initialize(hour, minute)
-    @hour = hour
-    @minute = minute
+  def initialize(hour: 0, minute:)
+    @minute = hour * MINUTES_PER_HOUR + minute
   end
 
   def to_s
-    "%02d:%02d" % [hour, minute]
+    "%02d:%02d" % minute.divmod(MINUTES_PER_HOUR)
   end
 
   def +(minutes)
-    hours, minutes = parse(minutes)
-    hours -= 24 if hour + hours >= 24
-    self.class.new(hour + hours, minute + minutes)
+    self.class.new(minute: (minute + minutes) % MINUTES_PER_DAY)
   end
 
   def -(minutes)
-    hours, minutes = parse(minutes)
-    hours -= 24 if hour - hours < 0
-    if minutes > minute
-      hours += 1
-      minutes -= 60
-    end
-    self.class.new(hour - hours, minute - minutes)
+    self + -minutes
   end
 
-  def ==(other_clock)
-    hour == other_clock.hour && minute == other_clock.minute
+  def ==(other)
+    minute == other.minute
   end
 
   protected
 
-  attr_reader :hour, :minute
-
-  private
-
-  def parse(minutes)
-    [minutes / 60, minutes.remainder(60)]
-  end
+  attr_reader :minute
 end
