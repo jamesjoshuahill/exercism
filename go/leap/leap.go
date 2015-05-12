5cbc63b84078da77f59f92a138d95506202632ca
Josh Hill <jamesjoshuahill@gmail.com>
2015-05-12 14:44:49 +0100

    Third iteration of go/leap

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,16 +1,24 @@
 package leap
 
-type Year int
-
-func (year Year) isMultipleOf(divisor int) bool {
-	return int(year)%divisor == 0
-}
-
 func IsLeapYear(n int) bool {
 	year := Year(n)
-	fourthYear := year.isMultipleOf(4)
-	century := year.isMultipleOf(100)
-	quadricentennial := year.isMultipleOf(400)
+	return year.isLeapYear()
+}
 
-	return fourthYear && (quadricentennial || !century)
+type Year int
+
+func (year Year) isFourthYear() bool {
+	return int(year)%4 == 0
+}
+
+func (year Year) isCentury() bool {
+	return int(year)%100 == 0
+}
+
+func (year Year) isQuadricentennial() bool {
+	return int(year)%400 == 0
+}
+
+func (year Year) isLeapYear() bool {
+	return year.isFourthYear() && (!year.isCentury() || year.isQuadricentennial())
 }
