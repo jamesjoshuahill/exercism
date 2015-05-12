7e3eebba423eb40a819eaad112cbd8d17ead929a
Josh Hill <jamesjoshuahill@gmail.com>
2015-05-12 12:57:35 +0100

    Second iteration on go/leap

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,17 +1,16 @@
 package leap
 
-import (
-	"math"
-)
+type Year int
 
-func IsLeapYear(year int) bool {
-	fourthYear := IsMultipleOf(4, year)
-	century := IsMultipleOf(100, year)
-	quadricentennial := IsMultipleOf(400, year)
-
-	return fourthYear && (quadricentennial || !century)
+func (year Year) isMultipleOf(divisor int) bool {
+	return int(year)%divisor == 0
 }
 
-func IsMultipleOf(y, x int) bool {
-	return math.Remainder(float64(x), float64(y)) == 0
+func IsLeapYear(n int) bool {
+	year := Year(n)
+	fourthYear := year.isMultipleOf(4)
+	century := year.isMultipleOf(100)
+	quadricentennial := year.isMultipleOf(400)
+
+	return fourthYear && (quadricentennial || !century)
 }
