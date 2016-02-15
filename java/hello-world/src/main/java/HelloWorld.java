0de64863a51152ee06a5d96bdc9e935fbb3b3490
Josh Hill <jamesjoshuahill@gmail.com>
2016-02-15 11:18:04 +0000

    Complete java hello world

--- java/hello-world/src/main/java/HelloWorld.java
+++ java/hello-world/src/main/java/HelloWorld.java
@@ -1,2 +1,8 @@
 public class HelloWorld {
+    public static String hello(String name) {
+        if (name == "" || name == null) {
+            name = "World";
+        }
+        return String.format("Hello, %s!", name);
+    }
 }
