ad8a496d1e0aaae28eac59c66d9eaee211c81c66
Josh Hill <jamesjoshuahill@gmail.com>
2016-02-23 17:18:20 +0000

    First version of etl in java

--- java/etl/src/main/java/Etl.java
+++ java/etl/src/main/java/Etl.java
@@ -1,8 +1,17 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Etl {
    public Map<String, Integer> transform(Map<Integer, List<String>> old) {
-      return null;
+       Map<String, Integer> transformed = new HashMap<>();
+
+       old.forEach((score, letters) -> {
+           letters.forEach((letter) -> {
+               transformed.put(letter.toString().toLowerCase(), score);
+           });
+       });
+
+       return transformed;
    }
 }
