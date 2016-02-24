e0c365e996631563faa0a5fe320b73d2e23298db
Josh Hill <jamesjoshuahill@gmail.com>
2016-02-24 16:57:11 +0000

    First iteration of java word count

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -1,2 +1,19 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class WordCount {
+    public Map<String,Integer> phrase(String phrase) {
+        List<String> words = Arrays.asList(phrase.split("\\W+"));
+        Map<String, Integer> counts = new HashMap<>();
+
+        words.forEach(word -> {
+            String key = word.toLowerCase();
+            Integer count = counts.containsKey(key) ? counts.get(key) + 1 : 1;
+            counts.put(key, count);
+        });
+
+        return counts;
+    }
 }
