0f67d613df88136f503879b8787157f7aec5fb7f
Josh Hill <jamesjoshuahill@gmail.com>
2016-02-25 12:00:46 +0000

    First iteration of java hamming

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,2 +1,15 @@
+import java.util.stream.IntStream;
+
 public class Hamming {
+    public static Integer compute(String strand, String other) {
+        if (strand.length() != other.length()) {
+            throw new IllegalArgumentException("Strands do not have equal length");
+        }
+
+        Long distance = IntStream.range(0, strand.length())
+                .filter(index -> strand.charAt(index) != other.charAt(index))
+                .count();
+
+        return distance.intValue();
+    }
 }
