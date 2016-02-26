2185fc33606073c2f3d28db9b3c8df1fcd540e05
Josh Hill <jamesjoshuahill@gmail.com>
2016-02-26 17:01:27 +0000

    Second iteration of java hamming

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -6,10 +6,14 @@ public class Hamming {
             throw new IllegalArgumentException("Strands do not have equal length");
         }
 
-        Long distance = IntStream.range(0, strand.length())
-                .filter(index -> strand.charAt(index) != other.charAt(index))
-                .count();
+        Integer distance = IntStream.range(0, strand.length())
+                .reduce(0, (count, index) -> {
+                    if (strand.charAt(index) != other.charAt(index)) {
+                        count++;
+                    }
+                    return count;
+                });
 
-        return distance.intValue();
+        return distance;
     }
 }
