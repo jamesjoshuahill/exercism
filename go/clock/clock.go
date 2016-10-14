80ff258fe80cde82a31f7da7ce8dc6f176f6fbf2
Josh Hill <jhill@pivotal.io>
2016-10-14 16:55:52 +0100

    First iteration of go/clock

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,34 +1,34 @@
 package clock
 
-import (
-  "fmt"
-)
+import "fmt"
 
 const TestVersion = 2
 
 type Clock struct {
-  hours int
-  minutes int
-}
-
-func (clock Clock) String() string {
-  return fmt.Sprintf("%02v:%02v", clock.hours, clock.minutes)
-}
-
-func (clock Clock) Add(minutes int) Clock {
-  return clock
+	hours   int
+	minutes int
 }
 
 func Time(hours, minutes int) Clock {
-  hours += minutes / 60
-  minutes = minutes % 60
-  hours = hours % 24
-  if minutes < 0 {
-    minutes += 60
-    hours -= 1
-  }
-  if hours < 0 {
-    hours += 24
-  }
-  return Clock{hours, minutes}
+	if hours < 0 {
+		return Time(hours+24, minutes)
+	}
+	if hours >= 24 {
+		return Time(hours-24, minutes)
+	}
+	if minutes < 0 {
+		return Time(hours-1, minutes+60)
+	}
+	if minutes >= 60 {
+		return Time(hours+1, minutes-60)
+	}
+	return Clock{hours, minutes}
+}
+
+func (c Clock) String() string {
+	return fmt.Sprintf("%02d:%02d", c.hours, c.minutes)
+}
+
+func (c Clock) Add(minutes int) Clock {
+	return Time(c.hours, c.minutes+minutes)
 }
