791396fa103cf596a11a7c23303527e56462842f
Josh Hill <jhill@pivotal.io>
2016-10-25 18:15:28 +0100

    First iteration of go/raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,7 +1,34 @@
 package raindrops
 
+import "fmt"
+
 const testVersion = 2
 
-func Convert(int) string
+func Convert(number int) string {
+	return drop{number}.toString()
+}
 
-// The test program has a benchmark too.  How fast does your Convert convert?
+type drop struct {
+	number int
+}
+
+func (d drop) toString() string {
+	var rain string
+	if d.multipleOf(3) {
+		rain += "Pling"
+	}
+	if d.multipleOf(5) {
+		rain += "Plang"
+	}
+	if d.multipleOf(7) {
+		rain += "Plong"
+	}
+	if rain == "" {
+		return fmt.Sprintf("%d", d.number)
+	}
+	return rain
+}
+
+func (d drop) multipleOf(x int) bool {
+	return d.number%x == 0
+}
