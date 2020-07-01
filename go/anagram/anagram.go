3e7930a1a27ca222c5a371841ef571616820b63b
Josh Hill <jamesjoshuahill@users.noreply.github.com>
2020-07-01 13:48:15 +0100

    Fourth iteration of go/anagram
    - Compare a prime product of the runes in subject and candidate strings
    to detect anagrams. This is ~800 ns/op faster than counting runes.
    - Use a switch statement to lookup the prime number for each rune
    because it is significantly faster than map lookup.

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -7,24 +7,82 @@ import (
 
 // Detect returns a list of candidates that are anagrams of subject.
 func Detect(subject string, candidates []string) []string {
-	count := countRunes(subject)
+	p := product(subject)
 
 	var anagrams []string
 	for _, c := range candidates {
 		if len(subject) != len(c) || strings.EqualFold(subject, c) {
 			continue
 		}
-		if count == countRunes(c) {
+		if p == product(c) {
 			anagrams = append(anagrams, c)
 		}
 	}
 	return anagrams
 }
 
-func countRunes(s string) [26]int {
-	var count [26]int
+func product(s string) int {
+	p := 1
 	for _, r := range s {
-		count[unicode.ToLower(r)-'a']++
+		p *= prime(unicode.ToLower(r))
 	}
-	return count
+	return p
+}
+
+func prime(r rune) int {
+	switch r {
+	case 'a':
+		return 2
+	case 'b':
+		return 3
+	case 'c':
+		return 5
+	case 'd':
+		return 7
+	case 'e':
+		return 11
+	case 'f':
+		return 13
+	case 'g':
+		return 17
+	case 'h':
+		return 19
+	case 'i':
+		return 23
+	case 'j':
+		return 29
+	case 'k':
+		return 31
+	case 'l':
+		return 37
+	case 'm':
+		return 41
+	case 'n':
+		return 43
+	case 'o':
+		return 47
+	case 'p':
+		return 53
+	case 'q':
+		return 59
+	case 'r':
+		return 61
+	case 's':
+		return 67
+	case 't':
+		return 71
+	case 'u':
+		return 73
+	case 'v':
+		return 79
+	case 'w':
+		return 83
+	case 'x':
+		return 89
+	case 'y':
+		return 97
+	case 'z':
+		return 101
+	}
+	return 0
 }
