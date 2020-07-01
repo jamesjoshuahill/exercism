e6413c656f25e79a400d12f5e6e3e27086a3e05a
Josh Hill <jamesjoshuahill@users.noreply.github.com>
2020-07-01 13:16:53 +0100

    Third iteration of go/anagram
    - Refactor anagram algorithm to count runes in the subject and
    candidate.
    - Interesting: '==' operator is defined for arrays, but not slices.

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -1,45 +1,30 @@
 package anagram
 
 import (
-	"sort"
 	"strings"
 	"unicode"
-	"unicode/utf8"
 )
 
 // Detect returns a list of candidates that are anagrams of subject.
 func Detect(subject string, candidates []string) []string {
-	sorted := sortRunes(subject)
+	count := countRunes(subject)
 
 	var anagrams []string
 	for _, c := range candidates {
 		if len(subject) != len(c) || strings.EqualFold(subject, c) {
 			continue
 		}
-		r := sortRunes(c)
-		if isEqual(sorted, r) {
+		if count == countRunes(c) {
 			anagrams = append(anagrams, c)
 		}
 	}
 	return anagrams
 }
 
-func sortRunes(s string) []rune {
-	runes := make([]rune, utf8.RuneCountInString(s))
-	for i, r := range s {
-		runes[i] = unicode.ToLower(r)
+func countRunes(s string) [26]int {
+	var count [26]int
+	for _, r := range s {
+		count[unicode.ToLower(r)-'a']++
 	}
-	sort.Slice(runes, func(i, j int) bool {
-		return runes[i] < runes[j]
-	})
-	return runes
-}
-
-func isEqual(a, b []rune) bool {
-	for i, r := range b {
-		if a[i] != r {
-			return false
-		}
-	}
-	return true
+	return count
 }
