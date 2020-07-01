168804b042009a5c665384c08b3527d76d3caa97
Josh Hill <jamesjoshuahill@users.noreply.github.com>
2020-07-01 13:09:05 +0100

    Second iteration of go/anagram
    - Use strings.EqualFold for case-insensitive comparision of subject and
    candidate strings. It saved ~100 allocations per operation.

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -2,45 +2,37 @@ package anagram
 
 import (
 	"sort"
+	"strings"
 	"unicode"
 	"unicode/utf8"
 )
 
 // Detect returns a list of candidates that are anagrams of subject.
 func Detect(subject string, candidates []string) []string {
-	lowercaseSubject := lowercaseRunes(subject)
-	sortedSubject := lowercaseRunes(subject)
-	sortRunes(sortedSubject)
+	sorted := sortRunes(subject)
 
 	var anagrams []string
 	for _, c := range candidates {
-		if len(subject) != len(c) {
+		if len(subject) != len(c) || strings.EqualFold(subject, c) {
 			continue
 		}
-		r := lowercaseRunes(c)
-		if isEqual(lowercaseSubject, r) {
-			continue
-		}
-		sortRunes(r)
-		if isEqual(sortedSubject, r) {
+		r := sortRunes(c)
+		if isEqual(sorted, r) {
 			anagrams = append(anagrams, c)
 		}
 	}
 	return anagrams
 }
 
-func lowercaseRunes(s string) []rune {
+func sortRunes(s string) []rune {
 	runes := make([]rune, utf8.RuneCountInString(s))
 	for i, r := range s {
 		runes[i] = unicode.ToLower(r)
 	}
-	return runes
-}
-
-func sortRunes(runes []rune) {
 	sort.Slice(runes, func(i, j int) bool {
 		return runes[i] < runes[j]
 	})
+	return runes
 }
 
 func isEqual(a, b []rune) bool {
